1fff6acc54997ef059afc696f365841dcf17d482
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-07-25 22:28:06 +0100

    fi

---
+++ component/Header.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <Image width='200' height='100' src='/logo.png'/>
+    </div>
+  )
+}
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import Layout from '@/component/Layout'
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Layout>
+    <Component {...pageProps} />
+  </Layout>
+}
