f50d3947b95eb40a9bac3d5ad04966b1bafaaa70
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-10-16 01:15:46 +0100

    sen

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -17,7 +17,7 @@ export default function LandingPage() {
           <center>
             <div className="about_head soooop">
               <p className="about_head_text">
-                <span className="ought"> Helping </span> Realtors Close Deals  Doing Less
+                <span className="ought"> Helping </span> Important Real estate Businesses <br/> Close more Deals , Doing Less
                 
               </p>
             </div>
