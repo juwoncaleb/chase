44a82c0e2874770934ed6233222c868ea1f48123
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-10-17 00:24:14 +0100

    kl

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['ik.imagekit.io'], // Add the hostname here
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+    },
  
 }
 
