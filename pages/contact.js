025ee0ec5621a33176abfea204faca3dc589a4fd
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-11-08 22:23:46 +0000

    ap

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Header from "@/component/Header";
 import Footer from "@/component/Footer";
+import { Widget } from '@typeform/embed-react'
 
 export default function Contact() {
   return (
@@ -21,77 +22,10 @@ export default function Contact() {
         <div className="bg-black form_fel flex justify-between">
          
 
-          <div>
-            <div className="flex ">
-              [01]
-              <p className="ml-6 qies">What is your name ?</p>
-            </div>
-            <input
-              type="text"
-              class="custom-input"
-              placeholder="Type your full name"
-            />
+        <Widget id="j14YAmqh"  className="widget" />
 
-            <div className="flex forn_det">
-              [02]
-              <p className="ml-6 qies">What is your email address ?</p>
-            </div>
-            <input
-              type="text"
-              class="custom-input"
-              placeholder="example@email.com"
-            />
 
-            <div className="flex forn_det">
-              [03]
-              <p className="ml-6 qies">What is your phone number ?</p>
-            </div>
-            <input type="text" class="custom-input" placeholder="Number" />
-
-            <div className="flex forn_det">
-              [04]
-              <p className="ml-6 qies">
-                What is your Real estate Business name?
-              </p>
-            </div>
-            <input
-              type="text"
-              class="custom-input"
-              placeholder="Business Name"
-            />
-
-            <div className="flex forn_det">
-              [04]
-              <p className="ml-6 qies">What is your project deadline?</p>
-            </div>
-            <input type="text" class="custom-input" placeholder="Deadline" />
-
-            <div className="flex forn_det">
-              [05]
-              <p className="ml-6 qies">
-                Briefly tell us some specific needs about your project
-              </p>
-            </div>
-
-            <input
-              type="text"
-              class="custom-input"
-              placeholder="Specific Needs"
-            />
-            <div>
-              <button className="sub_btn mt-10">Send Message</button>
-            </div>
-          </div>
-
-          <div className="tabb">
-            <p>CALL US</p>
-            <p>+234-816-235-3857</p>
-
-            <p className="mt-14">Email Address</p>
-            <p>Calebomojuwon@gmail.com</p>
-            <p className="mt-20">Office Address</p>
-            <p>Ikoyi, Lagos Nigeria </p>
-          </div>
+          
         </div>
       </div>
       <Footer />
