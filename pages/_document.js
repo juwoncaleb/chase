b1f8e1e5ef19a209b5882ee31872f9aedfae0dc9
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2025-07-02 02:30:34 +0100

    dd

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,29 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/lz.ico" />
+      <title>Chase Inc</title>
+      <meta name="description" content="Interior design marketing." />
+
+      {/* OG Tags for WhatsApp / Facebook */}
+      <meta property="og:title" content="Interior Design Agency in Lagos" />
+      <meta
+        property="og:description"
+        content="We help interior designers get luxury clients."
+      />
+      <meta property="og:image" content="https://ik.imagekit.io/juwoncaleb/sloo_db56g1lM-.png?updatedAt=1751419701012" />
+      <meta property="og:url" content="http://www.chaseinc.co/" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter card (optional) */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Interior Design Agency in Lagos" />
+      <meta
+        name="twitter:description"
+        content="We help interior designers get luxury clients."
+      />
+      <meta name="twitter:image" content="https://ik.imagekit.io/juwoncaleb/sloo_db56g1lM-.png?updatedAt=1751419701012" />
+     
       <link
         href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&family=Della+Respira&family=Italiana&display=swap"
         rel="stylesheet"
@@ -24,9 +47,10 @@ export default function Document() {
         href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;1,100;1,700&display=swap"
         rel="stylesheet"
       />
-      <link href="https://fonts.googleapis.com/css2?family=Goudy+Bookletter+1911&family=Ysabeau+Infant:ital,wght@0,1;0,100;0,200;0,300;0,400;0,500;1,1;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
-
-      </link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Goudy+Bookletter+1911&family=Ysabeau+Infant:ital,wght@0,1;0,100;0,200;0,300;0,400;0,500;1,1;1,100;1,200;1,300;1,400&display=swap"
+        rel="stylesheet"
+      ></link>
       <body>
         <Main />
         <NextScript />
